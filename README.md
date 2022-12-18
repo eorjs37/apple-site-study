6417f6d591cf75c72593a587ebf9d02cb33265dc
maxgunchoi <maxgunchoi@carrotglobal.com>
2022-12-19 08:47:46 +0900

    스크롤 이동에 따라 현재 scene 높이 구하기

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # apple-clone-site 공부
 
+## Section 높이 동적으로 구하기
+
+> window.innerHeight을 이용하여 각 sections을 동적으로 구하기
+
+```javascript
+const objs = [
+  {
+    section: document.querySelector("#section1"),
+  },
+  {
+    section: document.querySelector("#section2"),
+  },
+  {
+    section: document.querySelector("#section3"),
+  },
+  {
+    section: document.querySelector("#section4"),
+  },
+];
+const setSzie = () => {
+  const windowHeight = window.innerHeight;
+  objs.forEach((obj) => {
+    const { section } = obj;
+    section.style.height = `${windowHeight}px`;
+  });
+};
+window.addEventListener("resize", setSzie);
+```
+
 ## 현재 scene 구하기
 
 ## 현재 scene의 위치
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,51 @@
 /**
  * 즉시 실행함수
  */
-(() => {})();
+(() => {
+  const objs = [
+    {
+      section: document.querySelector("#section1"),
+    },
+    {
+      section: document.querySelector("#section2"),
+    },
+    {
+      section: document.querySelector("#section3"),
+    },
+    {
+      section: document.querySelector("#section4"),
+    },
+  ];
+
+  const setSzie = () => {
+    const windowHeight = window.innerHeight;
+    objs.forEach((obj) => {
+      const { section } = obj;
+      section.style.height = `${windowHeight}px`;
+    });
+  };
+
+  window.onload = () => {
+    setSzie();
+  };
+
+  window.addEventListener("resize", setSzie);
+  window.addEventListener("scroll", () => {
+    let prevScrollHeight = 0;
+    let currentScene = 1;
+    // for (let objInex = 0; objInex < currentScene; objInex++) {
+    //   const { section } = objs[objInex];
+    //   const pageYOffset = window.pageYOffset;
+    //   prevScrollHeight += section.clientHeight;
+
+    //   console.log("prevScrollHeight : ", prevScrollHeight);
+    //   console.log("pageYOffset : ", pageYOffset);
+    //   if (pageYOffset >= prevScrollHeight) {
+    //     currentScene++;
+    //     break;
+    //   }
+    // }
+
+    console.log(currentScene);
+  });
+})();
