9ddc58e5dfedfeb0cb0c7199a2a67503bf523b95
DESKTOP-OAJQPHK\user <maxgunchoi@carrotglobal.com>
2023-01-16 08:32:31 +0900

    현재 scene 구하기

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # apple-clone-site 공부
+
+## 현재 scene 구하기
--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,38 @@
 /**
  * 즉시 실행함수
  */
-(() => {})();
+(() => {
+  const container = [
+    {
+      section: document.querySelector("#section1"),
+    },
+    {
+      section: document.querySelector("#section2"),
+    },
+    {
+      section: document.querySelector("#section3"),
+    },
+    {
+      section: document.querySelector("#section4"),
+    },
+  ];
+  let currentScene = 0;
+
+  const setSize = () => {
+    for (let i = 0; i < container.length; i++) {
+      const { section } = container[i];
+      section.style.height = `${window.innerHeight}px`;
+    }
+
+    //현재 scene구하기
+  };
+
+  const setCurScene = () => {};
+
+  window.addEventListener("scroll", () => {
+    console.log(window.pageYOffset);
+    console.log(window.innerHeight);
+  });
+  window.addEventListener("resize", setSize);
+  window.addEventListener("DOMContentLoaded", setSize);
+})();
